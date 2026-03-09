e1bc5986ce1f120ffa2ce5cd837b92c2ab15330f
AlecKinnear <AlecKinnear@users.noreply.github.com>
2026-03-09 22:02:56 +0100

    Remove Swift app; keep Rust implementation only

--- UniversalScrobbler/UniversalScrobbler/Resources/MediaRemote.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-extern NSString *kMRMediaRemoteNowPlayingInfoDidChangeNotification;
-
-extern CFDictionaryRef MRMediaRemoteGetNowPlayingInfo(void);
-
-extern void MRMediaRemoteRegisterForNowPlayingNotifications(dispatch_queue_t queue);
-
-extern void MRMediaRemoteUnregisterForNowPlayingNotifications(void);
